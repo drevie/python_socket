4d68b6a91d345c3e3aaf6b31d65304feddfd42e2
Daniel Revie <daniel.revie1@gmail.com>
2017-02-22 10:50:52 -0500

    first commit

---
+++ sock352.py
@@ -0,0 +1,52 @@
+
+import binascii
+import socket as syssock
+import struct
+import sys
+
+# these functions are global to the class and
+# define the UDP ports all messages are sent
+# and received from
+
+
+def init(UDPportTx, UDPportRx):   # initialize your UDP socket here
+    # Create a UDP/Datagram Socket
+    udp_sock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
+    # Bind the port to the Rx (receive) port number
+    print UDPportRx
+    server_address = (UDPportRx, 80)
+    udp_sock.bind(server_address)
+    pass
+
+
+class socket:
+
+    def __init__(self):  # fill in your code here
+        return
+
+    def bind(self, address):
+        # NULL FUNCTION FOR PART 1 
+        return
+
+    def connect(self, address):  # fill in your code here
+        self.syssock.connect(address)
+
+        return
+
+    def listen(self, backlog):
+        return
+
+    def accept(self):
+        (clientsocket, address) = (1, 1)  # change this to your code
+        return (clientsocket, address)
+
+    def close(self):   # fill in your code here
+        return
+
+    def send(self, buffer):
+        bytessent = 0     # fill in your code here
+        return bytessent
+
+    def recv(self, nbytes):
+        bytesreceived = 0     # fill in your code here
+        return bytesreceived
