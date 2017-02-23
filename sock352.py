1d3ec2d811069e6c2f9f7318337a822e127e2a56
Daniel Revie <daniel.revie1@gmail.com>
2017-02-22 23:04:35 -0500

    adding pseudo

--- sock352.py
+++ sock352.py
@@ -15,17 +15,25 @@ SOCK352_HAS_OPT = 0xA0
 # these functions are global to the class and
 # define the UDP ports all messages are sent
 # and received from
+s = None
 
 
 def init(UDPportTx, UDPportRx):   # initialize your UDP socket here
-    # Create a UDP/Datagram Socket
-    udp_sock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
-    # Bind the port to the Rx (receive) port number
-    print UDPportRx
-    server_address = (UDPportRx, 8080)
-    udp_sock.bind(server_address)
-    pass
+    if UDPportRx < 0 or UDPportRx > 65535:
+        return -1
 
+    if UDPportTx < 0 or UDPportTx > 65535:
+        return -1
+
+    if UDPportRx == 0:
+        UDPportRx = 27182
+    if UDPportTx == 0:
+        UDPportTx = 27182
+
+    s = socket()
+
+    s.bind(('', UDPportRx))
+    return 1
 
 class socket:
 
@@ -59,26 +67,19 @@ class socket:
         udpPkt_hdr_data = struct.Struct(sock352PktHdrData)
         header = udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, header_len,
             checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len)
-        version = 0x1
-
+        
         while True:
-            # add the packet to the outbound queue
+            # add packet to outbound queue
             s.sendto(header, address)
-            print 'message sent'
-            # set the timeout 
-            #sendSock.settimeout(0,2)
-            try:
-                data, server = s.recvfrom(1024)
-                # wait for the return SYN
-                a, flags, c, d, e, f, g, i, ack, k, l = struct.unpack(sock352PktHdrData, data)
-                if flags & 0x1 == 1 and ack == 1:
-                    print 'connected'
-                    return 
 
-                break
+        # set the timeout
 
-            self.socketconnect(address)
-            return 1
+        #   wait for the return SYN
+
+        #       if there was a timeout, retransmit the SYN packet
+
+        # set the outbound and inbound sequence number
+
 
         return
 
@@ -95,6 +96,33 @@ class socket:
 
     def send(self, buffer):
         bytessent = 0     # fill in your code here
+
+        sequence_no = random.randint(0, 1000)
+        # Set Version to 0x1
+        version = 0x1
+        # protocol, opt_ptr, source_port, dest_port must be set to 0
+        flags = SOCK352_SYN
+        opt_ptr = 0
+        checksum = 0
+        protocol = 0
+        source_port = 0
+        dest_port = 0
+        # Next
+        ack_no = sequence_no + 1
+        window = 0
+        payload_len = 0
+        sock352PktHdrData = '!BBBBHHLLQQLL'
+        header_len = struct.calcsize(sock352PktHdrData)
+        udpPkt_hdr_data = struct.Struct(sock352PktHdrData)
+        header = udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, header_len,
+            checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len)
+        
+        # Create a new UDP packet with the header and buffer 
+        # Send the UDP packet to the destinatoin and transmit port
+        # Set the timeout
+        # wait or check for the ack or timeout
+
+
         return bytessent
 
     def recv(self, nbytes):
